1276f2438f8605542c92b109a143a3f857ee471f
miha2158 <miha.vanya@gmail.com>
2017-01-10 19:24:33 +0500

    Console menu
    ...

---
+++ HMenu/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            string[,] items = new string[,] { { "Один", "Два", "Три" }, { "Четыре", "Пять", "Шесть" }, { "Семь", "Восемь", "Девять" } };
+
+            int currentIndexX = 0, previousIndexX = 0;
+            int currentIndexY = 0, previousIndexY = 0;
+            int positionX = 5, positionY = 2;
+            bool itemSelected = false;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.CursorTop = positionY + i;
+                    Console.CursorLeft = positionX + j * 10;
+                    Console.ForegroundColor = ConsoleColor.Gray; Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(items[i , j]);
+                }
+
+            do
+            {
+                Console.CursorLeft = positionX + previousIndexX * 10;
+                Console.CursorTop = positionY + previousIndexY;
+                Console.ForegroundColor = ConsoleColor.Gray; Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(items[previousIndexY , previousIndexX]);
+
+
+                Console.CursorLeft = positionX + currentIndexX * 10;
+                Console.CursorTop = positionY + currentIndexY;
+                Console.ForegroundColor = ConsoleColor.Black; Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Write(items[currentIndexY , currentIndexX]);
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                previousIndexX = currentIndexX;
+                previousIndexY = currentIndexY;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        currentIndexX++;
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        currentIndexX--;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        currentIndexY--;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        currentIndexY++;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        itemSelected = true;
+                        break;
+                }
+
+                if (currentIndexX == 3)
+                {
+                    currentIndexX = 0;
+                    currentIndexY++;
+                }
+                else if (currentIndexX < 0)
+                {
+                    currentIndexX = 2;
+                    currentIndexY--;
+                }
+
+                if (currentIndexY == 3)
+                    currentIndexY = 0;
+                else if (currentIndexY < 0)
+                    currentIndexY = 2;
+
+            }
+            while (!itemSelected);
+
+            Console.CursorLeft = positionX;
+            Console.CursorTop = 15;
+
+            Console.WriteLine("Выбран пункт: {0}.", currentIndexY * 3 + currentIndexX + 1);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ VMenu/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            string[] items = {"Один", "Два", "Три"};
+
+            int[] lengths = new int[items.Length];
+            for(int i = 0; i < items.Length; i++)
+                switch (i)
+                {
+                    case 0:
+                        lengths[i] = 0;
+                        break;
+
+                    default:
+                        lengths[i] = lengths[i - 1] + items[i - 1].Length + 1;
+                        break;
+
+                }
+
+            int currentIndex = 0, previousIndex = 0;
+            int positionX = 5, positionY = 2;
+            bool itemSelected = false;
+                       
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.CursorLeft = positionX + lengths[i];
+                Console.CursorTop = positionY;
+                Console.ForegroundColor = ConsoleColor.Gray; Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(items[i]);
+            }
+
+            do
+            {
+                Console.CursorLeft = positionX + lengths[previousIndex];
+                Console.CursorTop = positionY;
+                Console.ForegroundColor = ConsoleColor.Gray; Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(items[previousIndex]);
+                
+                Console.CursorLeft = positionX + lengths[currentIndex];
+                Console.CursorTop = positionY;
+                Console.ForegroundColor = ConsoleColor.Black; Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Write(items[currentIndex]);           
+                
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                previousIndex = currentIndex;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        currentIndex++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        currentIndex--;
+                        break;
+                    case ConsoleKey.Enter:
+                        itemSelected = true;
+                        break;
+                }
+
+                if (currentIndex == items.Length)
+                    currentIndex = items.Length - 1;
+                else if (currentIndex < 0)
+                    currentIndex = 0;
+            }
+            while (!itemSelected);
+
+            Console.CursorLeft = positionX;
+            Console.CursorTop = 10;
+
+            Console.WriteLine("Выбран пункт: {0}.", currentIndex + 1);
+            Console.ReadLine();
+        }
+    }
+}
